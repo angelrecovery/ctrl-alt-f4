d1f51574b49d74aa3bacc246de73a266992181bf
a <missfox0@icloud.com>
2025-04-17 21:09:59 -0700

    refactor: clean up `req_kill` function

--- src/main.rs
+++ src/main.rs
@@ -18,12 +18,10 @@ fn top_hwnd() -> Option<HWND> {
 
 fn handle_from_hwnd(window: HWND) -> Option<HANDLE> {
     let mut pid = 0;
-
     unsafe {
         if GetWindowThreadProcessId(window, Some(&mut pid)) == 0 {
             return None;
         }
-
         OpenProcess(PROCESS_TERMINATE, false, pid).ok()
     }
 }
@@ -36,9 +34,7 @@ fn kill_process(handle: HANDLE) -> Result<()> {
 }
 
 fn req_kill() -> bool {
-    const KEY_COMBO: [u16; 3] = [VK_CONTROL.0, VK_MENU.0, VK_F4.0];
-
-    KEY_COMBO
+    [VK_CONTROL.0, VK_MENU.0, VK_F4.0]
         .iter()
         .all(|&vk| (unsafe { GetAsyncKeyState(vk.into()) }) & i16::MIN != 0)
 }
